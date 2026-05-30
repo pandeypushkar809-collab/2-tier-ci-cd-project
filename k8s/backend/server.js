c1367913806b3279fd712fff18f51a0db18d0fa3
Pushkar <pandeypushkar809@gmail.com>
2026-05-30 12:01:20 +0530

    Added EKS project files and documentation

---
+++ k8s/backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Backend Running Successfully"
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
